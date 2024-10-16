f395948d7f2896fbf1e9dc79aa755a0775f3a802
RichardCernansky <richard.cernansky3@gmail.com>
2024-10-16 23:54:52 +0200

    lets drop

--- processAST/main.py
+++ processAST/main.py
@@ -36,6 +36,7 @@ def extract_function_names(file_path):
     return function_names
 
 def save_functions_to_ndjson(node_tree: NodeTree, ascii_tree):
+    #print(ascii_tree) #ascii tree for debug prints
     """Save the entire tree as a single JSON object in NDJSON format."""
     with open(ndjson_path, "a") as f:
         #write to dictionary functions.ndjson if is function and is not main
@@ -83,8 +84,8 @@ def process_c_file(line: str):
 
 #-------------------------------------------------------------------------------------------------------------------
 #CSV process
-def process_file_csv(csv_file_path: str, file_name_column: str):
-    print(f"    Processing: {csv_file_path} with file_name_column: {file_name_column}")
+def process_file_csv(csv_file_path: str):
+    print(f"    Processing: {csv_file_path}.")
 
     # Open the CSV file
     with open(csv_file_path, mode='r', encoding='utf-8') as file:
@@ -97,20 +98,20 @@ def process_file_csv(csv_file_path: str, file_name_column: str):
 
     print(f"        Finished processing: {csv_file_path}. Success rate: {round(num_successful_rows/num_all_rows_c *100, 2)}%. N.o. rows in csv: {num_all_rows_c}.")
 
-def process_folder_csv(folder, file_name_column):
-    print(f"Processing folder: {folder} with file_name_column: {file_name_column}")
+def process_folder_csv(folder):
+    print(f"Processing folder: {folder}.")
     # Loop through each .csv file in the folder
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(".csv"):
                 csv_file_path = os.path.join(root, file)
-                process_file_csv(str(csv_file_path), file_name_column)
+                process_file_csv(str(csv_file_path))
 
 #CSV process
 #----------------------------------------------------------------------------------------------------------------------
 #JSONL process
 
-def process_file_jsonl(jsonl_file_path, file_name_column):
+def process_file_jsonl(jsonl_file_path):
     print(f"Processing JSONL file: {jsonl_file_path}")
 
     # Open the .jsonl file
@@ -122,7 +123,7 @@ def process_file_jsonl(jsonl_file_path, file_name_column):
 
                 # Extract the 'text' and 'func_def' fields
                 if 'text' in json_data and 'func_def' in json_data['text']:
-                    func_def_value = json_data['text'][file_name_column]
+                    func_def_value = json_data['text']['func_def']
                     process_c_file(func_def_value)
                 else:
                     print(f"'text' or 'func_def' key not found in the line.")
@@ -133,7 +134,7 @@ def process_file_jsonl(jsonl_file_path, file_name_column):
 
     print(f"        Finished processing: {jsonl_file_path}. Success rate: {round(num_successful_rows / num_all_rows_c * 100, 2)}%. N.o. rows in csv: {num_all_rows_c}.")
 
-def process_folder_jsonl(folder, file_name_column):
+def process_folder_jsonl(folder):
     print(f"Processing folder: {folder}")
 
     # Loop through each .jsonl file in the folder
@@ -143,7 +144,7 @@ def process_folder_jsonl(folder, file_name_column):
                 # Construct the full file path
                 jsonl_file_path = os.path.join(root, file)
                 # Process each jsonl file
-                process_file_jsonl(jsonl_file_path, file_name_column)
+                process_file_jsonl(jsonl_file_path)
 
 #JSONL process
 #--------------------------------------------------------------------------------------------------------------
@@ -153,7 +154,6 @@ def get_args():
 
     # Define expected arguments
     parser.add_argument("folder", help="Folder path")
-    parser.add_argument("file_name_column", help="File name column")
     parser.add_argument("file_type", help="Type of file: 'csv' or 'json'")
 
     return parser.parse_args()
@@ -163,15 +163,14 @@ def main():
 
     # Access arguments directly by their name
     folder = args.folder
-    file_name_column = args.file_name_column
     file_type = args.file_type
 
     # Ensure the folder exists
     if os.path.exists(folder):
         if file_type == 'csv':
-            process_folder_csv(folder, file_name_column)
+            process_folder_csv(folder)
         elif file_type == 'json':
-            process_folder_jsonl(folder, file_name_column)
+            process_folder_jsonl(folder)
         else:
             print(f"Error: Unsupported file type '{file_type}'. Use 'csv' or 'json'.")
     else:
