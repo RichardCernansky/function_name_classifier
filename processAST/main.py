b700e6fd917d49b90ea4a6ca9f768f6875d24f63
RichardCernansky <richard.cernansky3@gmail.com>
2024-10-18 09:46:00 +0200

    docu

--- processAST/main.py
+++ processAST/main.py
@@ -5,9 +5,15 @@ import subprocess
 import sys
 import json
 import re
+import hashlib
 
 from AsciiTreeProcessor import AsciiTreeProcessor
 from NodeTree import NodeTree
+
+# this is a program, that generates .ndjson file from dataset specified as parameter
+# every line in .ndjson file represents tree structure of distinct string function
+# (i.e. no function snippet occurs more than once in the .ndjson file - initial filtering)
+# the functions' trees are obtained from ascii tree representation of every .c code snippet in the datasets
 
 #consts
 ndjson_path = "functionsASTs.ndjson"
@@ -17,6 +23,12 @@ csv.field_size_limit(sys.maxsize)
 
 num_all_rows_c = 0
 num_successful_rows = 0
+
+# get hash for seen_func_set
+def get_md5_hash(input_string: str) -> str:
+    md5_object = hashlib.md5()
+    md5_object.update(input_string.encode('utf-8'))
+    return md5_object.hexdigest()
 
 #function to extract function from string stored in file_path
 def extract_function_names(file_path):
@@ -61,30 +73,37 @@ def run_cnip() -> subprocess.CompletedProcess[str]:
     command = f"./psychec/cnip -l C -d {temp_file_path}"
     return subprocess.run(command, shell=True, capture_output=True, text=True, encoding='ISO-8859-1')
 
-def process_c_file(line: str):
+def process_c_file(line: str, seen_func_hashes: set):
 
     global num_all_rows_c, num_successful_rows
     num_all_rows_c += 1
 
-    with open(temp_file_path, 'w') as temp_file:
-        # Write the cleaned content to the temp file
-        temp_file.write(line)
+    func_hash = get_md5_hash(line)
+    if line not in seen_func_hashes:
+        seen_func_hashes.add(func_hash)
 
-    result = run_cnip() #------------------------------------------RUN--------------------
+        with open(temp_file_path, 'w') as temp_file:
+            # Write the cleaned content to the temp file
+            temp_file.write(line)
 
-    # check exitcode, if error -> thrash the tree (don't save it)
-    if result.returncode != 0:
-        pass
-    # if successful, process the ascii-tree
+        result = run_cnip() #------------------------------------------RUN--------------------
+
+        # check exitcode, if error -> thrash the tree (don't save it)
+        if result.returncode != 0:
+            pass
+        # if successful, process the ascii-tree
+        else:
+            num_successful_rows += 1
+            ascii_to_ndjson(result.stdout)
+        return
     else:
-        num_successful_rows += 1
-        ascii_to_ndjson(result.stdout)
-    return
+        print("Repeated function found.")
+        pass #don't do anything with function we already saw
 
 
 #-------------------------------------------------------------------------------------------------------------------
 #CSV process
-def process_file_csv(csv_file_path: str):
+def process_file_csv(csv_file_path: str, seen_func_hashes: set):
     print(f"    Processing: {csv_file_path}.")
 
     # Open the CSV file
@@ -94,24 +113,24 @@ def process_file_csv(csv_file_path: str):
         for line in reader:
             # Check if the specified filename_column ends with '.c'
             if line["file"].endswith('.c'):
-                process_c_file(line["flines"])
+                process_c_file(line["flines"], seen_func_hashes)
 
     print(f"        Finished processing: {csv_file_path}. Success rate: {round(num_successful_rows/num_all_rows_c *100, 2)}%. N.o. rows in csv: {num_all_rows_c}.")
 
-def process_folder_csv(folder):
+def process_folder_csv(folder, seen_func_hashes: set):
     print(f"Processing folder: {folder}.")
     # Loop through each .csv file in the folder
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(".csv"):
                 csv_file_path = os.path.join(root, file)
-                process_file_csv(str(csv_file_path))
+                process_file_csv(str(csv_file_path), seen_func_hashes)
 
 #CSV process
 #----------------------------------------------------------------------------------------------------------------------
 #JSONL process
 
-def process_file_jsonl(jsonl_file_path):
+def process_file_jsonl(jsonl_file_path, seen_func_hashes: set):
     print(f"Processing JSONL file: {jsonl_file_path}")
 
     # Open the .jsonl file
@@ -124,7 +143,7 @@ def process_file_jsonl(jsonl_file_path):
                 # Extract the 'text' and 'func_def' fields
                 if 'text' in json_data and 'func_def' in json_data['text']:
                     func_def_value = json_data['text']['func_def']
-                    process_c_file(func_def_value)
+                    process_c_file(func_def_value, seen_func_hashes)
                 else:
                     print(f"'text' or 'func_def' key not found in the line.")
 
@@ -134,7 +153,7 @@ def process_file_jsonl(jsonl_file_path):
 
     print(f"        Finished processing: {jsonl_file_path}. Success rate: {round(num_successful_rows / num_all_rows_c * 100, 2)}%. N.o. rows in csv: {num_all_rows_c}.")
 
-def process_folder_jsonl(folder):
+def process_folder_jsonl(folder, seen_func_hashes: set):
     print(f"Processing folder: {folder}")
 
     # Loop through each .jsonl file in the folder
@@ -144,7 +163,7 @@ def process_folder_jsonl(folder):
                 # Construct the full file path
                 jsonl_file_path = os.path.join(root, file)
                 # Process each jsonl file
-                process_file_jsonl(jsonl_file_path)
+                process_file_jsonl(jsonl_file_path, seen_func_hashes)
 
 #JSONL process
 #--------------------------------------------------------------------------------------------------------------
@@ -165,12 +184,13 @@ def main():
     folder = args.folder
     file_type = args.file_type
 
-    # Ensure the folder exists
+    seen_func_hashes = set()
+    # ensure the folder exists
     if os.path.exists(folder):
         if file_type == 'csv':
-            process_folder_csv(folder)
+            process_folder_csv(folder, seen_func_hashes)
         elif file_type == 'json':
-            process_folder_jsonl(folder)
+            process_folder_jsonl(folder, seen_func_hashes)
         else:
             print(f"Error: Unsupported file type '{file_type}'. Use 'csv' or 'json'.")
     else:
