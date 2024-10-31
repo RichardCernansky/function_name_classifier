7dab746a975f3b272e832b90fe42fd0cad587a65
RichardCernansky <richard.cernansky3@gmail.com>
2024-10-31 16:55:17 +0100

    m

--- training_pipeline/extract_functions/tmp/tempSourceCode.c
+++ training_pipeline/extract_functions/tmp/tempSourceCode.c
@@ -1,100 +1,145 @@
-#include <stdlib.h>
- #include <stdio.h>
- #include <string.h>
+/* 
+  * File:   main.c
+  * Author: 
+  *
+  * Created on April 30, 2017, 10:50 AM
+  */
  
- typedef struct lst_element lstElement;
- struct lst_element {
- 	char *name;
- 	lstElement *next;
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ struct core_info {
+     double propability;
+     unsigned char aided;
  };
  
- lstElement *first, *now;
+ int compareProbabilities(const void* a, const void* b) {
+     const double* da = (const double*) a;
+     const double* db = (const double*) b;
  
- int main(int argc, char *argv[]) {
- 	FILE *in, *out;
- 	in = fopen(argv[1], "r");
- 	out = fopen("output.out", "w");
- 
- 	int N;
- 	fscanf(in, "%i\n", &N);
- 	
- 	int n;
- 	for (n = 0; n < N; n++) {
- 		int S;
- 		fscanf(in, "%i\n", &S);
- 		
- 		int s;
- 		char *buffer = malloc(sizeof(char) * 100);
- 		for (s = 0; s < S; s++) {
- 			fgets(buffer, 100, in);
- 		}
- 		
- 		int Q;
- 		fscanf(in, "%i\n", &Q);
- 
- 		first = malloc(sizeof(lstElement));
- 		first->next = NULL;
- 		first->name = malloc(sizeof(char) * 100);
- 		fgets(buffer, 100, in);
- 		strcpy(first->name, buffer);
- 		int anzahl = 1;
- 		int wechsel = 0;
- 
- //printf("query: %s\t\tanzahl: %i\n", buffer, anzahl);
- 		int q;
- 		for (q = 1; q < Q; q++) {
- 			fgets(buffer, 100, in);
- 
- 			lstElement *tmp = first;
- 			lstElement *ins = NULL;
- 			while (tmp) {
- 				if (strcmp(tmp->name, buffer)) {
- 					ins = tmp;
- 					tmp = tmp->next;
- 				} else
- 					break;
- 			}
- 
- 			if (tmp == NULL) {
- 				ins->next = malloc(sizeof(lstElement));
- 				tmp = ins->next;
- 				tmp->next = NULL;
- 				tmp->name = malloc(sizeof(char) * 100);
- 				
- 				strcpy(tmp->name, buffer);
- 
- 				anzahl++;
- 			}
- 
- 			if (anzahl == S) {
- //printf("---> Wechsel\n");
- 				wechsel++;
- 				anzahl = 1;
- 
- 				tmp = first;
- 				while (tmp) {
- 					lstElement *del = tmp;
- 					tmp = tmp->next;
- 					free(del->name);
- 					free(del);
- 				}
- 
- 				first = malloc(sizeof(lstElement));
- 				first->next = NULL;
- 				first->name = malloc(sizeof(char) * 100);
- 				strcpy(first->name, buffer);			
- 			}
- //printf("query: %s\t\tanzahl: %i\n\t\t[%i/%i]\n", buffer, anzahl, q+1, Q);
- 		}
- 
- 		lstElement *tmp = first;
- 		while (tmp) {
- 			lstElement *del = tmp;
- 			tmp = tmp->next;
- 			free(del->name);
- 			free(del);
- 		}
- 
- 		fprintf(out, "Case #%i: %i\n", n+1, wechsel);
- 	}
+     if (*da > *db)
+         return 1;
+     if (*da < *db)
+         return -1;
+     return 0;
  }
+ 
+ void get_cores(unsigned* total, unsigned * required) {
+     scanf("%d %d", total, required);
+ }
+ 
+ void get_tests(unsigned* tests) {
+     scanf("%d", tests);
+ }
+ 
+ void get_trainings(double* trainings) {
+     scanf("%lf", trainings);
+ }
+ 
+ void get_probabilities(double* probabilities, unsigned count) {
+     unsigned p;
+ 
+     for (p = 0; p < count; ++p) {
+         scanf("%lf", probabilities + p);
+     }
+ 
+     qsort(probabilities, count, sizeof (double), compareProbabilities);
+ }
+ 
+ void give_aid_to_cores(double* cores, unsigned count, double training) {
+     while (training > 0) {
+         double needs = 0;
+         unsigned c, ct;
+ 
+         for (c = 1, ct = 1; c < count; ++c) {
+             if (cores[c] > cores[c - 1]) {
+                 needs = cores[c] - cores[c - 1];
+                 break;
+             }
+             ++ct;
+         }
+ 
+         if (needs == 0)
+             break;
+ 
+         needs *= ct;
+         if (needs > training) {
+             needs = training;
+         }
+         training -= needs;
+         needs /= ct;
+ 
+         for (c = 0; c < ct; ++c) {
+             cores[c] += needs;
+         }
+ 
+ /*
+         for (c = 0; c < count; ++c) {
+             printf("%.4f ", cores[c]);
+         }
+         puts("");
+ */
+     }
+ 
+     if (training > 0) {
+         unsigned c;
+         double needs = 1 - cores[0];
+         
+         needs *= count;
+         if (needs > training)
+             needs = training;
+         needs /= count;
+         
+         for (c = 0; c < count; ++c) {
+             cores[c] += needs;
+         }
+ 
+ /*
+         for (c = 0; c < count; ++c) {
+             printf("%.4f ", cores[c]);
+         }
+         puts("");
+ */
+     }
+ }
+ 
+ double give_final_result(double* cores, unsigned count) {
+     double result = 1;
+     unsigned c;
+ 
+     for (c = 0; c < count; ++c) {
+         result *= cores[c];
+     }
+ 
+     return result;
+ }
+ 
+ /*
+  * 
+  */
+ int main(int argc, char** argv) {
+     unsigned tests, total_cores, required_cores;
+     double trainings;
+     double* cores;
+ 
+     unsigned t;
+ 
+     get_tests(&tests);
+ 
+     for (t = 0; t < tests; ++t) {
+         get_cores(&total_cores, &required_cores);
+         get_trainings(&trainings);
+ 
+         cores = malloc(sizeof (double) * total_cores);
+         get_probabilities(cores, total_cores);
+ 
+         give_aid_to_cores(cores, total_cores, trainings);
+ 
+         printf("Case #%d: %f\n", t + 1, give_final_result(cores, total_cores));
+ 
+         free(cores);
+     }
+ 
+     return (EXIT_SUCCESS);
+ }
+ 
