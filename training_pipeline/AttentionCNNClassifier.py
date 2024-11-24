3c09c6bf0b10b74acadda14b5fb6fbafec7ab0c9
Richard Cernansky <richard.cernansky3@gmail.com>
2024-11-24 10:07:09 +0100

    m

--- training_pipeline/AttentionCNNClassifier.py
+++ training_pipeline/AttentionCNNClassifier.py
@@ -265,7 +265,7 @@ validation_dataset = dataset_valid.take(number_lines_valid)
 batch_logger = LambdaCallback(on_batch_end=on_batch_end)
 history = model.fit(
     train_dataset,
-    epochs=1,
+    epochs=10,
     steps_per_epoch=steps_per_epoch,
     validation_data=validation_dataset,
     validation_steps=validation_steps,
