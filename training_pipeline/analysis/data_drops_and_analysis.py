38e05f45d48fcbe10edb5c75614b56eb327fa8e3
Richard Cernansky <richard.cernansky3@gmail.com>
2024-12-10 22:43:06 +0100

    m

--- training_pipeline/analysis/data_drops_and_analysis.py
+++ training_pipeline/analysis/data_drops_and_analysis.py
@@ -15,9 +15,9 @@ ast_depths_prefix = "exploratory_analysis/ast_depths/"
 num_nodes_prefix = "exploratory_analysis/num_nodes/"
 
 pdf_postfix = ".pdf"
-input_ndjson_file = "../data_ndjson/gcj-dataset.ndjson"
+#input_ndjson_file = "../data_ndjson/gcj-dataset.ndjson"
 #input_ndjson_file = "../data_ndjson/contests.ndjson"
-# input_ndjson_file = "../data_ndjson/merged.ndjson"
+input_ndjson_file = "../data_ndjson/merged.ndjson"
 output_ndjson_file = "../data_ndjson/dropped_lower_5.ndjson"
 basename_without_extension = get_basename_without_extension(input_ndjson_file)
 
