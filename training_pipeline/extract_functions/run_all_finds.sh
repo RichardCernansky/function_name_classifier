17d2bfc8658447e93b63fe65d9d183f3de0493ca
Richard Cernansky <richard.cernansky3@gmail.com>
2024-12-13 10:52:50 +0100

    m

---
+++ training_pipeline/extract_functions/run_all_finds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python ./find_function.py /home/richardce/codeforces-dataset/contests language source_code --find_name dbsortfncsj
+python ./find_function.py /home/richardce/codeforces-dataset/contests language source_code --find_name llpow
+python ./find_function.py /home/richardce/codeforces-dataset/contests language source_code --find_name dbzt
+python ./find_function.py /home/richardce/codeforces-dataset/contests language source_code --find_name llcm 
