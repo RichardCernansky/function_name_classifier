e80911a09bd7c17f724c84cc5462da03966c4837
RichardCernansky <richard.cernansky3@gmail.com>
2025-02-14 11:30:25 +0100

    extract functions with author

--- training_pipeline/extract_functions/main.py
+++ training_pipeline/extract_functions/main.py
@@ -20,8 +20,9 @@ from NodeTree import NodeTree
 folder = None
 file_name_col = None
 code_snip_col = None
+author_col = None
 
-#consts
+# consts
 ndjson_path = "../data_ndjson/"
 temp_file_path = "tmp/tempSourceCode.c"
 ndjson_suffix = ".ndjson"
@@ -32,12 +33,14 @@ csv.field_size_limit(sys.maxsize)
 num_all_rows_c = 0
 num_successful_rows = 0
 
-seen_func_strings= set()
-def save_functions_to_ndjson(node_tree: NodeTree, ndjson_path_t):
-    #print(ascii_tree) #ascii tree for debug prints
+seen_func_strings = set()
+
+
+def save_functions_to_ndjson(node_tree: NodeTree, ndjson_path_t: str, author_name: str):
+    # print(ascii_tree) #ascii tree for debug prints
     """Save the entire tree as a single JSON object in NDJSON format."""
     with open(ndjson_path_t, "a") as f:
-        #write to dictionary functions.ndjson if is function and is not ['main', 'solve']
+        # write to dictionary functions.ndjson if is function and is not ['main', 'solve']
         for child in node_tree.root_node.children:
             if child.kind == "FunctionDefinition":
                 definition_node = child
@@ -50,7 +53,7 @@ def save_functions_to_ndjson(node_tree: NodeTree, ndjson_path_t):
                                 declarator_child.data = "?"
                                 func_tree_dict = definition_node.to_dict()
                                 json_data = {
-                                    "tag": tag,
+                                    "tag": tag if author_name == "" else author_name,
                                     "num_tokens": AsciiTreeProcessor.get_num_tokens(definition_node),
                                     "ast_depth": AsciiTreeProcessor.get_ast_depth(definition_node),
                                     "num_nodes": AsciiTreeProcessor.get_num_nodes(definition_node),
@@ -61,17 +64,20 @@ def save_functions_to_ndjson(node_tree: NodeTree, ndjson_path_t):
                                 break
                         break
 
-def ascii_to_ndjson(ascii_tree: str):
+
+def ascii_to_ndjson(ascii_tree: str, author_name: str):
     """Convert an ASCII tree into NDJSON format."""
     atp = AsciiTreeProcessor(ascii_tree)
     node_tree = NodeTree(atp.produce_tree())
     global ndjson_path
-    save_functions_to_ndjson(node_tree, ndjson_path)
+    save_functions_to_ndjson(node_tree, ndjson_path, author_name)
+
 
 def run_cnip(prefix) -> subprocess.CompletedProcess[str]:
     """Run the CNIP command to generate the ASCII tree."""
     command = f"{prefix}psychec/cnip -l C -d {prefix}{temp_file_path}"
     return subprocess.run(command, shell=True, capture_output=True, text=True, encoding='ISO-8859-1')
+
 
 def extract_func_body(content, match):
     start = match.start()
@@ -85,7 +91,8 @@ def extract_func_body(content, match):
         end += 1
     return content[start:end]
 
-def process_c_file(line_func: str):
+
+def process_c_file(line_func: str, author_name: str):
     global num_all_rows_c, num_successful_rows, seen_func_strings
 
     function_pattern = re.compile(
@@ -105,15 +112,15 @@ def process_c_file(line_func: str):
 
             result = run_cnip("./")
 
-
             if result.returncode == 0 and result.stdout.strip():
                 num_successful_rows += 1
-                ascii_to_ndjson(result.stdout)
+                ascii_to_ndjson(result.stdout, author_name)
             else:
                 print(f"Error processing function:\n{func_string}")
 
-#-------------------------------------------------------------------------------------------------------------------
-#CSV process
+
+# -------------------------------------------------------------------------------------------------------------------
+# CSV process
 def process_file_csv(csv_file_path: str):
     print(f"    Processing: {csv_file_path}.")
 
@@ -123,11 +130,14 @@ def process_file_csv(csv_file_path: str):
 
         for line in reader:
             # check if the specified filename_column ends with '.c', !!!Don't use .lower() for .C because some .C are cpp!!!
-            if line[file_name_col] is not None and (line[file_name_col].endswith('.c') or line[file_name_col].lower().endswith('gnu c')):
-                process_c_file(line[code_snip_col])
+            if line[file_name_col] is not None and (
+                    line[file_name_col].endswith('.c') or line[file_name_col].lower().endswith('gnu c')):
+                process_c_file(line[code_snip_col], line[author_col])
 
     success_rate = round(num_successful_rows / num_all_rows_c * 100, 2) if num_all_rows_c > 0 else 0
-    print(f"        Finished processing: {csv_file_path}. Success rate: {success_rate}%. N.o. '.c' rows in csv: {num_all_rows_c}.")
+    print(
+        f"        Finished processing: {csv_file_path}. Success rate: {success_rate}%. N.o. '.c' rows in csv: {num_all_rows_c}.")
+
 
 def process_folder_csv(folder):
     print(f"Processing folder: {folder}.")
@@ -138,8 +148,9 @@ def process_folder_csv(folder):
                 csv_file_path = os.path.join(root, file)
                 process_file_csv(str(csv_file_path))
 
-#CSV process
-#----------------------------------------------------------------------------------------------------------------------
+
+# CSV process
+# ----------------------------------------------------------------------------------------------------------------------
 
 def get_args():
     parser = argparse.ArgumentParser(description="Process files in a folder")
@@ -148,18 +159,21 @@ def get_args():
     parser.add_argument("folder", help="Folder path")
     parser.add_argument("file_name_col", help="File_name column name in csv")
     parser.add_argument("code_snip_col", help="Code_snippet column name in csv")
-
+    parser.add_argument("author_col", type=str, help="Author's name (requires --author)")
 
     return parser.parse_args()
 
+
 def main():
-    global folder, file_name_col, code_snip_col, ndjson_path  # Declare global variables
+    global folder, file_name_col, code_snip_col, ndjson_path, author_col  # Declare global variables
 
     args = get_args()
 
     folder = args.folder
     file_name_col = args.file_name_col
     code_snip_col = args.code_snip_col
+    author_col = args.author_col
+
     ndjson_path = ndjson_path + os.path.basename(folder) + ndjson_suffix
     print(ndjson_path)
 
@@ -168,7 +182,8 @@ def main():
     else:
         print(f"Error: Folder not found: {folder}")
 
-#usage for gcj - main.py gcj file flines
-#usage for codeforces - main.py codeforces language source_code
+
+# usage for gcj - main.py gcj file flines
+# usage for codeforces - main.py codeforces language source_code
 if __name__ == "__main__":
     main()
--- training_pipeline/extract_functions/tmp/tempSourceCode.c
+++ training_pipeline/extract_functions/tmp/tempSourceCode.c
@@ -1,23 +1,77 @@
-//
-//  func.c
-//  
-//
-//  Created by MacBook return 0; on 11.11.14.
-//
-//
-
-#include <stdio.h>
-
-int main(void)
-{
-    unsigned long long n;
-    long long res;
-    scanf("%I64d", &n);
-    if (n%2) {
-        res = -((n + 1)/2);
-    } else {
-        res = n/2;
-    }
-    printf("%I64d", res);
-    return 0;
-}
+ 
+ int main(int argc , char *argv[])
+ {
+ 	FILE *ifp;
+ 
+ 	int i,j,k,l,ncase,nserver,nswitch=0,nq,count;
+ 	char querry[102];
+ 	if(argc != 2)
+ 	{
+ 		printf("Error:\n");
+ 		printf("Usage: filename <input file>\n");
+ 		printf("Example: A-small.out A-small.in\n");
+ 		exit(1);
+ 	}
+ 	ifp = fopen(argv[1] , "r");
+ 	if( ifp == NULL )
+ 	{
+ 		printf("error in opening file");
+ 	}
+ 
+ 	// Read from the file 
+ 	
+ 	fscanf(ifp,"%d",&ncase);
+ 	Server *sname = NULL;
+ 	for(i = 0; i < ncase ; i++)
+ 	{
+ 		fscanf(ifp,"%d",&nserver);
+ 		fgetc(ifp);
+ 		sname = (Server *) calloc(nserver,sizeof(Server));
+ 		//. create memory for server names
+ 		nswitch=0;
+ 		count=nserver;
+ 		for( j = 0 ; j < nserver ;j++)
+ 		{
+ 			
+ 			fgets(sname[j].servername,101,ifp);
+ 			sname[j].flag = 0;
+ 		}
+ 
+ 		fscanf(ifp,"%d",&nq);
+ 		fgetc(ifp);
+ 		for( j = 0 ; j < nq ; j++)
+ 		{
+ 			fgets(querry,101,ifp);
+ 						
+ 			for(k = 0 ; k < nserver ; k++)
+ 			{
+ 				if(strcmp(querry,sname[k].servername) == 0)
+ 				{
+ 					if(sname[k].flag==0)
+ 					{
+ 						sname[k].flag++;
+ 						count--;
+ 						if(count==0)
+ 						{
+ 							nswitch++;
+ 							count = nserver-1;
+ 							for(l=0;l<nserver;l++)
+ 							{
+ 								if(l!=k)
+ 								{
+ 									sname[l].flag = 0;
+ 								}
+ 							}
+ 							
+ 						}
+ 					}
+ 					
+ 					break;
+ 				}
+ 			}
+ 		}
+ 		printf("Case #%d: %d\n",i+1,nswitch); 
+ 		free(sname);
+ 	}
+ 	return 0;
+ }
